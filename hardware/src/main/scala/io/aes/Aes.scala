72be59951a4b8fa464f9f63beca88248616e0993
tympanix <tympanix@gmail.com>
2019-05-03 15:59:47 +0200

    add: aes constants

--- hardware/src/main/scala/io/aes/Aes.scala
+++ hardware/src/main/scala/io/aes/Aes.scala
@@ -15,6 +15,26 @@ object Aes extends DeviceObject {
   trait Pins {}
 }
 
+// Constants for aes key lengths
+object AesKey {
+  val L128 = Bits("b00")
+  val L192 = Bits("b01")
+  val L256 = Bits("b10")
+}
+
+// Constants for aes mode of operation
+object AesMode {
+  val ECB = UInt(0, width = 8)
+}
+
+// Constants for aes local address space
+object AesAddr {
+  val CONF       = Bits("h0000")
+  val KEY        = Bits("h1000")
+  val BLOCK_IN   = Bits("h2000")
+  val BLOCK_OUT  = Bits("h3000")
+}
+
 class Aes() extends CoreDevice() {
 
   override val io = new CoreDeviceIO() with Aes.Pins
@@ -24,6 +44,10 @@ class Aes() extends CoreDevice() {
 
   // Register for requests from OCP master
   val masterReg = Reg(next = io.ocp.M)
+  
+  // Local address for core device
+  val localAddr = Bits(width = 16)
+  localAddr := masterReg.Addr(15,0)
 
   val key = Mem(UInt(width = 8), 32)
   val blockIn = Mem(UInt(width = 8), 16)
@@ -39,13 +63,22 @@ class Aes() extends CoreDevice() {
   when (masterReg.Cmd === OcpCmd.RD) {
     io.ocp.S.Resp := OcpResp.DVA
     io.ocp.S.Data := contentReg + 5.U
-
-    
   }
 
   // Handle OCP writes
   when (masterReg.Cmd === OcpCmd.WR) {
     io.ocp.S.Resp := OcpResp.DVA
     contentReg := masterReg.Data
+
+    // Write to aes input block
+    when(localAddr === AesAddr.BLOCK_IN) {
+      val local = masterReg.Addr - AesAddr.BLOCK_IN
+      for (i <- 0 until masterReg.ByteEn.getWidth) {
+        val addr = local + UInt(i)
+        when (masterReg.ByteEn(i) === Bits(1)) {
+          blockIn(addr) := masterReg.Data(8*i+7, 8*i)
+        }
+      }
+    }
   }
 }
